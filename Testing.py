9762ae6076b247bc25ad6cbd0e3cb250413a1f24
Nathan Steele <nathanjoshuasteele@gmail.com>
2019-02-11 14:09:42 +0000

    extracting health data

--- Testing.py
+++ Testing.py
@@ -1,100 +1,68 @@
 import json
-with open('../../user-site-export/steps-2019-01-10.json') as f:
-    data = json.load(f)
+
+def getJSON(filePath):
+    with open(filePath, 'r') as f:
+        return json.load(f)
 
 def getDateFromDateTime(dateTime):
     dateTimeArray = dateTime.split()
     return dateTimeArray[0]
 
-def maxStepsPerInterval():
+def maxDataInterval(specifiedData, specifiedDate):
+    jsonData = getJSON('../../user-site-export/' + specifiedData + '-' + specifiedDate + '.json')
     max = 0
-    for item in data['states']:
+    for item in jsonData['states']:
         steps = int(item['value'])
         if (steps >= max):
             max = steps
             datetime = item['dateTime']
-
     return (max, datetime)
+        
+
+'''
+{
+  "date" : "01/10/01",
+  "steps" : 17872,
+  "distance" : 6883,
+  "altitude" : 64,
+  "calories" : 4351
+}
 
 # print(len(data['states']))
+dailyData = dict()
 
+def extractDates(specifiedDate):
+    data = getJSON('../../user-site-export/steps-' + specifiedDate + '.json')
+    for date in data['states']:
+        dailyData['date'] = getDateFromDateTime(date['dateTime'])
+    
+    with open('all_health_data.json', 'w') as f:
+        json.dump(dailyData, f)
 
-dailySteps = dict()
+def extractRawData(specifiedData, specifiedDate):
+    data = getJSON('../../user-site-export/' + specifiedData + '-' + specifiedDate + '.json')
+    oldDateTime = specifiedDate
+    dailyDataSum = 0
 
-oldDateTime = '01/10/19'
-stepsInOneDay = 0
+    for item in data['states']:
+        newDateTime = getDateFromDateTime(item['dateTime'])
+        dataInterval = int(item['value'])
 
-for item in data['states']:
-    newDateTime = getDateFromDateTime(item['dateTime'])
-    stepsInterval = int(item['value'])
+        # we got to the end of a day
+        if (oldDateTime != newDateTime):
+            dailyData[specifiedData] = dailyDataSum
+            print(json.dumps(dailyData, indent=2))
 
-    if (oldDateTime != newDateTime):
-        dailySteps[stepsInOneDay] = oldDateTime
-        stepsInOneDay = 0
+            with open('all_health_data.json', 'w') as f:
+                json.dump(dailyData, f)
+            dailyDataSum = 0
+        
+        dailyDataSum += dataInterval
+        oldDateTime = getDateFromDateTime(item['dateTime'])
 
-    if (stepsInterval != 0):
-        stepsInOneDay += stepsInterval
-
-    oldDateTime = getDateFromDateTime(item['dateTime'])
-
-print(json.dumps(dailySteps, indent=2))
-print(maxStepsPerInterval())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-people_string = 
-{
-    "people": [
-        {
-            "name": "John Smith",
-            "phone": "615-555-7164",
-            "emails": ["js@gmail.com", "js@gmx.co.uk"],
-            "has_license": false
-        },
-        {
-            "name": "Jan Doe",
-            "phone": "560-555-5153",
-            "emails": null,
-            "has_license": true
-        }
-    ]
-}
-'''
-
-# data = json.loads(people_string)
-
-''' GET EVERYONES NAME
---------------------------------
-for person in data['people']:
-    print(person['name'])
-'''
-
-''' DELETE EVERYONES PHONE NUMBER (requires converting from string to python object)
----------------------------------
-for person in data['people']:
-    del person['phone']
-
-new_str = json.dumps(data, indent=2, sort_keys=True)        # dumps() converts strings to python objects. sort_keys is an optional parameter that sorts keys in alphabetical order
-print(new_str)
-'''
-
-''' (load json files into python objects, and then write those objects back to json files)
---------------------------------
+# extractDates('2019-01-10')
+extractRawData('steps', '2019-01-10')
+#print(json.dumps(dailyData, indent=2))
+#print(maxDataInterval('steps', '2019-01-10'))
 
 '''
